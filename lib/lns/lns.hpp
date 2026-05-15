6d3fefcf897dc16a08ff9d1ec1316643f7b87c55
UShouldRun <henriqueteixeira845@gmail.com>
2026-05-15 15:24:56 +0100

    v1.9.1 - changed funct3 and funct7 encodings for funct3 to dictate the type bit size and funct7 the R inst type

--- lib/lns/lns.hpp
+++ lib/lns/lns.hpp
@@ -70,38 +70,38 @@
 #define LNS32 0x02
 #define LNS64 0x03
 
-#define LNS_ADD_FUNCT3  0x0
-#define LNS_SUB_FUNCT3  0x1
-#define LNS_MUL_FUNCT3  0x2
-#define LNS_DIV_FUNCT3  0x3
-#define LNS_SQRT_FUNCT3 0x4
+#define LNS_ADD_FUNCT7  0x00 // 0b0000000
+#define LNS_SUB_FUNCT7  0x04 // 0b0000100
+#define LNS_MUL_FUNCT7  0x08 // 0b0001000
+#define LNS_DIV_FUNCT7  0x0C // 0b0001100
+#define LNS_SQRT_FUNCT7 0x10 // 0b0010000
 
-#define LNS_EQ_FUNCT3   0x5
-#define LNS_LT_FUNCT3   0x6
-#define LNS_LE_FUNCT3   0x7
+#define LNS_EQ_FUNCT7   0x14 // 0b0010100
+#define LNS_LT_FUNCT7   0x18 // 0b0011000
+#define LNS_LE_FUNCT7   0x1C // 0b0011100
 
-#define LNS_CVT_FUNCT3  0x1
+#define LNS_CVT_FUNCT7  0x20 // 0b0100000
 
-#define LNS_ADD(rd, rs1, rs2, funct7) \
-  CUSTOM_R(rd, rs1, rs2, CUSTOM0, LNS_ADD_FUNCT3, funct7)
-#define LNS_SUB(rd, rs1, rs2, funct7) \
-  CUSTOM_R(rd, rs1, rs2, CUSTOM0, LNS_SUB_FUNCT3, funct7)
-#define LNS_MUL(rd, rs1, rs2, funct7) \
-  CUSTOM_R(rd, rs1, rs2, CUSTOM0, LNS_MUL_FUNCT3, funct7)
-#define LNS_DIV(rd, rs1, rs2, funct7) \
-  CUSTOM_R(rd, rs1, rs2, CUSTOM0, LNS_DIV_FUNCT3, funct7)
-#define LNS_SQRT(rd, rs1, funct7) \
-  CUSTOM_R(rd, rs1, rs1, CUSTOM0, LNS_SQRT_FUNCT3, funct7)
+#define LNS_ADD(rd, rs1, rs2, funct3) \
+  CUSTOM_R(rd, rs1, rs2, CUSTOM0, funct3, LNS_ADD_FUNCT7)
+#define LNS_SUB(rd, rs1, rs2, funct3) \
+  CUSTOM_R(rd, rs1, rs2, CUSTOM0, funct3, LNS_SUB_FUNCT7)
+#define LNS_MUL(rd, rs1, rs2, funct3) \
+  CUSTOM_R(rd, rs1, rs2, CUSTOM0, funct3, LNS_MUL_FUNCT7)
+#define LNS_DIV(rd, rs1, rs2, funct3) \
+  CUSTOM_R(rd, rs1, rs2, CUSTOM0, funct3, LNS_DIV_FUNCT7)
+#define LNS_SQRT(rd, rs1, funct3) \
+  CUSTOM_R(rd, rs1, rs1, CUSTOM0, funct3, LNS_SQRT_FUNCT7)
 
-#define LNS_EQ(rd, rs1, rs2, funct7) \
-  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, LNS_EQ_FUNCT3, funct7)
-#define LNS_LT(rd, rs1, rs2, funct7) \
-  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, LNS_LT_FUNCT3, funct7)
-#define LNS_LE(rd, rs1, rs2, funct7) \
-  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, LNS_LE_FUNCT3, funct7)
+#define LNS_EQ(rd, rs1, rs2, funct3) \
+  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, funct3, LNS_EQ_FUNCT7)
+#define LNS_LT(rd, rs1, rs2, funct3) \
+  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, funct3, LNS_LT_FUNCT7)
+#define LNS_LE(rd, rs1, rs2, funct3) \
+  LNS_COMPARE(rd, rs1, rs2, CUSTOM0, funct3, LNS_LE_FUNCT7)
 
-#define LNS_CVT_F32(rd, rs1, funct7) \
-  CUSTOM_R(rd, rs1, rs1, CUSTOM2, LNS_CVT_FUNCT3, funct7)
+#define LNS_CVT_F32(rd, rs1, funct3) \
+  CUSTOM_R(rd, rs1, rs1, CUSTOM0, funct3, LNS_CVT_FUNCT7)
 
 #define LNS_LOAD_FUNCT3  0x5
 #define LNS_STORE_FUNCT3 0x6
